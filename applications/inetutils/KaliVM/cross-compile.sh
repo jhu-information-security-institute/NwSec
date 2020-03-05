395afd12f3210dcbfa467b2926eb1c70df01fa02
Reuben Johnston <reub@jhu.edu>
2020-03-05 10:07:07 -0500

    interim.

--- applications/inetutils/KaliVM/cross-compile.sh
+++ applications/inetutils/KaliVM/cross-compile.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-# Run from top-level of inetutils-1.9.4
+#Usage: Run from top-level of inetutils-1.9.4 and do not run as root
 
-export CROSS_COMPILE=/opt/gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-export ARCH=aarch64
+#This is probably unnecessary here (as we are using automake), but ...
+export CROSS_COMPILE=/opt/gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- && export ARCH=aarch64
+
+#Because inetutils uses automake, we have to configure with --host=aarch64-linux-gnu --build=x86_64-pc-linux-gnu
+#So, add the cross-compiler bin folder to PATH
+export PATH=$PATH:/opt/gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin
+
+./configure --host=aarch64-linux-gnu --build=x86_64-pc-linux-gnu
 autoreconf -f -i
-./configure
 make -j8
