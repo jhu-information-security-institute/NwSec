c51bd316f366b2e9700254fc83af418aabd260aa
Reuben Johnston <reub@jhu.edu>
2021-04-18 18:16:31 -0400

    updates

--- applications/emailsvr/UbuntuServerX86-64/postfix_install.sh
+++ applications/emailsvr/UbuntuServerX86-64/postfix_install.sh
@@ -1,4 +1,4 @@
 cat << EOF | debconf-set-selections 
 postfix postfix/main_mailer_type string 'Internet Site' 
-postfix postfix/mailname string 'mymail.netsec.isi.jhu.edu'
+postfix postfix/mailname string 'mail.nwsecdocker.jhu.edu'
 EOF
