2cf7dc5c3291bf9ba9013d9cae1a63a5f11b76ed
Reuben Johnston <reub@jhu.edu>
2021-04-21 10:42:25 -0400

    updates

--- config/network-ubuntu/install.sh
+++ config/network-ubuntu/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
+echo 'Making directory ~/netplan'
 mkdir -p ~/netplan
+echo 'Copying netplan scripts and configuration files to ~/netplan'
 cp etc_netplan_100_config.yaml ~/netplan/100_config.yaml
 cp prepshutdown_netplan.sh ~/netplan/.
 cp warmstart_netplan.sh ~/netplan/.
