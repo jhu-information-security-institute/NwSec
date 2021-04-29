203a4b76e73de0be5968bb13c18cb42a41528fdf
Reuben Johnston <reub@jhu.edu>
2021-04-29 08:36:32 -0400

    new selks files.

---
+++ applications/selkssvr/beats/downloadbeats.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.12.1-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.12.1-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.1-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-7.12.1-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.12.1-amd64.deb
+
