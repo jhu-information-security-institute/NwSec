0e3f69f9c6643d1271662d824bbb78556770f3df
reub <reub@jhu.edu>
2022-02-28 08:26:39 -0500

    updates.

--- os/apu4/openwrt/KaliX86-64/scripts/build.sh
+++ os/apu4/openwrt/KaliX86-64/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /sandbox
+cd /mnt/hgfs/sandbox
 
 git clone https://github.com/openwrt/openwrt.git
 
@@ -10,8 +10,6 @@ git checkout tags/v21.02.2
 #https://openwrt.org/docs/guide-developer/feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
-make menuconfig
 
 #https://www.pcengines.ch/wle600vx.htm
 #Qualcomm Atheros QCA9882
@@ -25,4 +23,8 @@ make menuconfig
 #    - s: stdout+stderr (equal to the old V=99)
 #    - c: commands (for build systems that suppress commands by default, e.g. kbuild, cmake)
 #    - w: warnings/errors only (equal to the old V=1)
-#make -j8 V=sc
+echo 'Create a default based on diffconfig by running $ cp /mnt/hgfs/scripts/diffconfig .config'
+echo 'Then expand to a full config by running $ make defconfig'
+echo 'Customize config by running $ make menuconfig'
+echo 'Customize kernel config by running $ make kernel_menuconfig'
+echo 'Build by running $ make -j $nproc) V=sc download clean world'
