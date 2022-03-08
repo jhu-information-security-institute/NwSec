a88dd4723d9e154ec1673e6dee274f36efb258ff
reub <reub@jhu.edu>
2022-03-08 08:51:58 -0500

    cleanup.

--- applications/dhcpsvr/dhcpsvr_UbuntuServerX86-64.sh
+++ applications/dhcpsvr/dhcpsvr_UbuntuServerX86-64.sh
@@ -1,8 +1,8 @@
-# Script to clone specific files for creating a suricata environment within a Docker container
+# Script to clone specific files for creating a dhcp server environment within a Docker container
 # running a minimal Ubuntu server image
 
 # Create a directory, so Git doesn't get messy, and enter it
-mkdir websvr && cd websvr
+mkdir dhcpsvr && cd dhcpsvr
 
 # Start a Git repository
 git init
@@ -20,15 +20,14 @@ git config core.sparseCheckout true
 # That is inside the hidden .git directory that was created
 # by running the command: git init
 # And inside it enter the name of the specific files (or subdirectory) you only want to clone
-echo 'applications/websvr/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
-echo 'applications/websvr/UbuntuServerX86-64/Dockerfile' >> .git/info/sparse-checkout
-echo 'applications/websvr/UbuntuServerX86-64/index.html' >> .git/info/sparse-checkout
-echo 'applications/websvr/UbuntuServerX86-64/etc_nginx_sites-available-nwsec' >> .git/info/sparse-checkout
+echo 'applications/dhcpsvr/UbuntuServerX86-64/.project' >> .git/info/sparse-checkout
+echo 'applications/dhcpsvr/UbuntuServerX86-64/Dockerfile' >> .git/info/sparse-checkout
+echo 'applications/dhcpsvr/UbuntuServerX86-64/etc_dhcp_dhcpd.conf' >> .git/info/sparse-checkout
 
 ## Download with pull, not clone
 git pull origin master
 
-echo 'cd into termsvr/applications/websvr/UbuntuServerX86-64 and view details in Dockerfile for building, running, and attaching to the container'
+echo 'cd into dhcpsvr/applications/dhcpsvr/UbuntuServerX86-64 and view details in Dockerfile for building, running, and attaching to the container'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
