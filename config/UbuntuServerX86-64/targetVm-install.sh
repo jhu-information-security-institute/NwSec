9cb2f8536972ac84a9144044334fd68e1114d6de
reubenajohnston <reub@jhu.edu>
2022-03-11 11:40:47 -0500

    Update targetVm-install.sh

--- config/UbuntuServerX86-64/targetVm-install.sh
+++ config/UbuntuServerX86-64/targetVm-install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-echo 'Making directoris in ~/netplan'
-mkdir -p ~/netplan
-mkdir -p ~/netplan/backup
-echo 'Backing up current configuration'
-mv /etc/netplan/* ~/netplan/backup
-echo 'Copying netplan scripts and configuration files to ~/netplan'
-cp etc_netplan_100_config.yaml ~/netplan/100_config.yaml
-cp prepshutdown_netplan.sh ~/netplan/.
-cp warmstart_netplan.sh ~/netplan/.
+#Must be run as root!
+#Backs up /etc/netplan and copies over configuration files into /etc/netplan
+echo 'Backup files in /etc/netplan'
+mkdir -p /etc/netplan/backup
+mv /etc/netplan/* /etc/netplan/backup
+echo 'Copying netplan scripts and configuration files to /etc/netplan'
+cp etc_netplan_100_config.yaml /etc/netplan/100_config.yaml
+cp etc_netplan_101_config.yaml /etc/netplan/101_config.yaml
