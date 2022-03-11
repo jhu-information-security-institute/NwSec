0feafd72dbea475da0aaea649682ef7b86c297fe
reubenajohnston <reub@jhu.edu>
2022-03-11 12:28:05 -0500

    Update infrastructureVm-remove.sh

--- config/UbuntuServerX86-64/infrastructureVm-remove.sh
+++ config/UbuntuServerX86-64/infrastructureVm-remove.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 echo 'Removing files'
-rm /etc/netplan/99_config.yaml
-rm /etc/netplan/100_config.yaml
-rm /etc/netplan/warmstart_netplan.sh
-rm /etc/netplan/prepshutdown_netplan.sh
+rm -Rf ~/netplan
+rm /etc/netplan/99-config.yaml
+rm /etc/netplan/100-config.yaml
 mv /etc/netplan/backup/* /etc/netplan
 rmdir /etc/netplan/backup
