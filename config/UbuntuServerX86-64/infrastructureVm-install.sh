02b17fc50e640bd1abc4d016f62046a3c205801f
reubenajohnston <reub@jhu.edu>
2022-03-11 11:38:58 -0500

    Update infrastructureVm-install.sh

--- config/UbuntuServerX86-64/infrastructureVm-install.sh
+++ config/UbuntuServerX86-64/infrastructureVm-install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
+#Must be run as root!
 echo 'Backup files in /etc/netplan'
 mkdir -p /etc/netplan/backup
 mv /etc/netplan/* /etc/netplan/backup
 echo 'Copying netplan scripts and configuration files to /etc/netplan'
 cp etc_netplan_99_config.yaml /etc/netplan/99_config.yaml
 cp etc_netplan_100_config.yaml /etc/netplan/100_config.yaml
-cp warmstart_netplan.sh ~/netplan/.
-cp prepshutdown_netplan.sh ~/netplan/.
+cp etc_netplan_warmstart_netplan.sh /etc/netplan
+cp etc_netplan_prepshutdown_netplan.sh /etc/netplan
