cbd6262023de743b16aadb8446f90622e6ee6478
reubenajohnston <reub@jhu.edu>
2022-03-11 23:49:07 -0500

    Update infrastructureVm-install.sh

--- config/UbuntuServerX86-64/infrastructureVm-install.sh
+++ config/UbuntuServerX86-64/infrastructureVm-install.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/bash
 #Must be run as root!
-#Backs up /etc/netplan and copies over configuration files and scripts into ~/netplan
+#Backs up /etc/netplan and copies over configuration files into /etc/netplan
 echo 'Backup files in /etc/netplan'
 mkdir -p /etc/netplan/backup
 mv /etc/netplan/* /etc/netplan/backup
-echo 'Copying netplan scripts and configuration files to ~/netplan'
-mkdir -p ~/netplan
+echo 'Copying netplan scripts to /etc/netplan'
 cp etc_netplan_99-config.yaml /etc/netplan/99-config.yaml
-cp etc_netplan_100-config.yaml ~/netplan/100-config.yaml
-cp warmstart-netplan.sh ~/netplan/warmstart-netplan.sh
-cp prepshutdown-netplan.sh ~/netplan/prepshutdown-netplan.sh
 echo 'Setup network-manager'
-sudo mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf  /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf_orig
-sudo touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
-sudo systemctl restart network-manager
+mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf  /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf_orig
+touch /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
+systemctl restart network-manager
