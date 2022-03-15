d50fb57044111f8633127200207132d8928898bf
reubenajohnston <reub@jhu.edu>
2022-03-15 09:54:38 -0400

    Update infrastructureVm-install1.sh

--- config/UbuntuServerX86-64/infrastructureVm-install1.sh
+++ config/UbuntuServerX86-64/infrastructureVm-install1.sh
@@ -3,7 +3,7 @@
 #Backs up /etc/netplan and copies over configuration files into /etc/netplan
 echo 'Backup files in /etc/netplan'
 mkdir -p /etc/netplan/backup
-mv /etc/netplan/* /etc/netplan/backup
+mv /etc/netplan/*.yaml /etc/netplan/backup
 echo 'Copying netplan scripts to /etc/netplan'
 cp etc_netplan_99-config.yaml /etc/netplan/99-config.yaml
 echo 'Setup network-manager'
