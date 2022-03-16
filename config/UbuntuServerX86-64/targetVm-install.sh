46c7f6fcb4004a2b6be2face5eaa8220ad10f6dc
reubenajohnston <reub@jhu.edu>
2022-03-15 23:24:51 -0400

    Update targetVm-install.sh

--- config/UbuntuServerX86-64/targetVm-install.sh
+++ config/UbuntuServerX86-64/targetVm-install.sh
@@ -7,5 +7,5 @@ mv /etc/netplan/*.yaml /etc/netplan/backup
 echo 'Copying netplan configuration files to /etc/netplan'
 cp etc_netplan_101-config.yaml /etc/netplan/101-config.yaml
 echo 'Disable network-manager'
-sudo systemctl stop NetworkManager
-sudo systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl disable NetworkManager
